d37f68e453e6c240d88e4d45a7b04d723b5f90ec
Andrew Withers <andreww@thomasdev.co.uk>
2017-08-24 12:56:26 +0100

    Add invoke-build base plumbing

---
+++ HelloWorld/README.txt
@@ -0,0 +1,6 @@
+
+To execute build, open PowerShell Interactive Window and run:
+
+> .\build.ps1			# run default / first task
+
+> .\build.pas init2		# run specific task named "init2""
---
+++ HelloWorld/build.ps1
@@ -0,0 +1,9 @@
+cls
+
+. '\functions.ps1'
+
+# identify latest version of invoke-build package by enumerating using wildcard query, and extract latest version full name (ie path)
+$invokeBuild = (Get-ChildItem('.\packages\Invoke-Build*\tools\Invoke-Build.ps1')).FullName | Sort-Object $_ | Select -Last 1
+
+# call latest invoke build passing through arguments given to this script, followed by the tasks script to be used for build action
+& $invokeBuild $args tasks.ps1
